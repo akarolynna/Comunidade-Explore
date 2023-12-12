28835dc842d52f9a5aeafe3567e2e6299ac6de89
carol-sf <annacarolina.franca21@gmail.com>
2023-12-12 09:33:45 -0300

    Feat: criando o menu

---
+++ View/pagina-inicial.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidade Explore</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href='../Public/CSS/PaginaInicial.css'>
+</head>
+<body>
+    <nav class="menu">
+        <div class="grupo">
+            <img src="../Public/Imagens/LogoExplore.png" alt="Logo de bussola" class="logo">
+            <h1 class="titulo">Comunidade Explore</h1>
+        </div>
+        <div class="grupo">
+            <button class="btn btn-success btn-sm botao">
+                Criar publicação
+            </button>
+            <img src="../Public/Imagens/ImagemUsuario.png" alt="Imagem do usuário" class="imagemUsuario">
+        </div>
+    </nav>
+</body>
+</html>
