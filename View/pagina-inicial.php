3e17c543e3a1fc6f4ba1d44652a25235cd307415
carol-sf <annacarolina.franca21@gmail.com>
2023-12-12 11:16:49 -0300

    Feat: adicionando o menu flutuante para realizar logout

--- View/pagina-inicial.php
+++ View/pagina-inicial.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comunidade Explore</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href='../Public/CSS/PaginaInicial.css'>
 </head>
 <body class="pagina">
@@ -17,7 +18,15 @@
             <button class="btn btn-success btn-sm botao">
                 Criar publicação
             </button>
-            <img src="../Public/Imagens/ImagemUsuario.png" alt="Imagem do usuário" class="imagemUsuario">
+
+            <div class="dropdown-menu-left">
+                <img src="../Public/Imagens/ImagemUsuario.png" alt="Imagem do usuário" class="imagemUsuario dropdown-toggle" id="imagemUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                
+                <div class="dropdown-menu" aria-labelledby="imagemUsuario">
+                    <a href="#" class="dropdown-item">Meu perfil</a>
+                    <a href="./logout.php" class="dropdown-item">Sair</a>
+                </div>
+            </div>
         </div>
     </nav>
 
@@ -80,4 +89,9 @@
         </div>
     </div>
 </body>
+
+<script src="../Public/JS/global.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
