336020482e9b77847afc231a0dcc6b3b0d40fe79
carol-sf <annacarolina.franca21@gmail.com>
2023-12-13 20:11:36 -0300

    Feat: criando o cartão de post

--- View/pagina-inicial.php
+++ View/pagina-inicial.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comunidade Explore</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href='../Public/CSS/PaginaInicial.css'>
 </head>
 <body class="pagina">
@@ -69,12 +71,40 @@
                 <p class="itemMenuPublicacoes titulo" id="eventos">Eventos</p>
                 <p class="itemMenuPublicacoes titulo" id="guias">Guias</p>
             </div>    
-            <div id="publicacoes"></div>
+            <div id="publicacoes">
+
+                <div class="post">
+                    <div class="imagemPost">
+                        <img src="../Public/Imagens/CategoriaPraia.jpg" alt="Imagem do Post">
+                    </div>
+                    <div class="conteudoPost">
+                        <div class="cabecalhoPost">
+                            <div class="criadorPost">
+                                <img src="../Public/Imagens/ImagemUsuario.png" alt="Foto do autor">
+                                <p>Anna Carolina</p>
+                            </div>
+                            <p>20 out 2023</p>
+                        </div>
+                        <h3 class="tituloPost mt-3">Título do Diario de Viagem</h3>
+                        <p class="mt-3 descricaoPost">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, laudantium iste similique non animi rerum maiores saepe. Tempora veritatis rerum, delectus dignissimos illum, eum quo sint perferendis ea veniam natus.
+                        </p>
+                        <div class="teste">
+                            <i class="corCinzaClaro cursorPointer far fa-heart fa-lg"></i>
+                            <p class="corCinzaClaro ml-2 mr-4">152</p>
+                            <i class="corCinzaClaro cursorPointer far fa-comment fa-lg"></i>
+                            <p class="corCinzaClaro ml-2 mr-4">152</p>
+                            <i class="corCinzaClaro cursorPointer far fa-bookmark fa-lg"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </body>
 
-<script src="../Public/JS/pagina-inicial.js"></script>
+<!-- <script src="../Public/JS/pagina-inicial.js"></script> -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
