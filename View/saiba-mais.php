05c84dd3ac69c796379650596d4144f2c251879a
carol-sf <annacarolina.franca21@gmail.com>
2023-12-13 21:01:42 -0300

    Feat: adicionando as páginas de apresentação da Janaína

---
+++ View/saiba-mais.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../Public/CSS/SaibaMais.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Seja Bem-Vindo</title>
+</head>
+<body>
+  <header>
+    <div class="menu">
+      <ul>
+          <li><a href="../index.php">Home</a></li>
+          <li><a href="./saiba-mais">Saiba Mais</a></li>
+          <li><a href="#">Desenvolvedores</a></li>
+      </ul>
+  </div>
+  </header>
+  <main>
+    <section class="conteudo_principal">
+      <div class="imagem">
+        <img src="../Public/Imagens/SaibaMais.jpg" alt="Imagem Principal">
+      </div>
+      <div class="conteudo">
+          <h2  class="subtitulo">Saiba Mais</h2>
+          <p class="texto">Estamos entusiasmados em apresentar nosso projeto em andamento, desenvolvido por estudantes dedicados do curso de Sistemas de Informação do IFF. 
+            Este é um espaço em evolução, concebido para ser mais do que apenas um site - é uma comunidade em crescimento para apaixonados por viagens.
+            <br>
+            Nossa missão é criar um ambiente acolhedor onde viajantes possam compartilhar experiências, descobrir novos destinos e conectar-se com pessoas de todo o mundo. 
+            Estamos trabalhando arduamente para reunir roteiros inspiradores, dicas valiosas e relatos autênticos que enriqueçam a experiência de cada explorador.
+            <br>
+            Ao acompanhar nosso progresso, você testemunhará a construção deste projeto, desde a fase inicial até a criação de uma comunidade vibrante. 
+            <strong>Junte-se a nós nessa jornada para explorar o mundo, compartilhar conhecimento e inspirar outros a descobrirem as maravilhas das viagens.</strong></p>
+      </div>
+  </section>
+  </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./Public/CSS/Index.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Seja Bem-Vindo</title>
+</head>
+<body>
+  <header>
+    <div class="menu">
+      <ul>
+          <li><a href="./index.php">Home</a></li>
+          <li><a href="./View/saiba-mais.php">Saiba Mais</a></li>
+          <li><a href="#">Desenvolvedores</a></li>
+      </ul>
+    </div>
+  </header>
+  <main>
+    <section class="conteudo_principal">
+      <div class="imagem">
+        <img src="./Public/Imagens/ApresentacaoSite.jpg" alt="Imagem Principal">
+      </div>
+      <div class="bemvindotext">
+          <h2  class="subtitulo">Seja Bem-Vindo</h2>
+          <p class="texto">Este é um espaço virtual para amantes de viagens. Aqui, compartilhamos experiências, dicas e histórias inspiradoras. 
+            Todos, de novatos a viajantes experientes, se reúnem para trocar ideias. 
+            Encontre roteiros, conselhos e relatos autênticos que alimentam a alma aventureira. 
+            Junte-se a nós para compartilhar suas próprias descobertas, desde dicas sobre cafés em Paris até registros de aventuras na Patagônia. 
+            Bem-vindo à nossa comunidade de viajantes, onde histórias fascinantes esperam por você. 
+            Aventure-se, explore e compartilhe!</p>
+          <a href="./View/login.php" class="btn botaoLogin mb-2">Login</a>
+          <a href="./View/cadastro.php" class="btn botaoCadastro mb-2">Cadastre-se</a>
+      </div>
+  </section>
+  </main>
+</body>
+</html>
