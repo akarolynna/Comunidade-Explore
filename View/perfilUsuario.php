72031e355f75cf6b2cc0f10e3c828e6634db46d3
Anna Karolynna <annakarolynnasouza8181@gmail.com>
2024-01-19 20:19:16 -0300

    HTML & CSS
    Criação estática da tela de Perfil do Usuário. Com descrição, cards, informações sobre o usuario, descrição.

--- View/perfilUsuario.php
+++ View/perfilUsuario.php
@@ -8,58 +8,99 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../Public/CSS/PerfilUsuario.css">
     <title>Meu Perfil</title>
-    <!-- <title>?php echo  $_SESSION['usuario']['nome']; ?></title> -->
 </head>
 
 <body>
-    <header>
-        <div class="logo">
-            <img src="../Public/Imagens/LogoExplore.png" alt="Logo de bussola">
-            <h6> Comunidade-Explore </h6>
-        </div>
-        <nav class="menu">
-            <ul>
-                <li><a href="#">Desafios</a></li>
-                <li><a href="#">Eventos</a></li>
-                <li><a href="#">Diários de Viagem</a></li>
-                <li><a href="#">Seguindo</a></li>
-            </ul>
-        </nav>
-    </header>
     <article>
-        <div class="informacoes-membro">
+        <div class="imagem-usuario">
             <div class="imagemPerfil">
                 <img src="<?php echo $_SESSION['usuario']['foto']; ?>" alt="Foto do usuário" class="imagemUsuario dropdown-toggle" id="imagemUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             </div>
-            <div class="informacoesUsuario">
-                <div class="tituloInformacoesUsuario">
-                    <h3>Anna Karolynna</h3><!-- <span>php echo  $_SESSION['usuario']['email']; ?></span> -->
-                </div>
+        </div>
+        <div class="informacoes-usuario">
+            <div class="tituloInformacoesUsuario">
+                <h3>Sobre: </h3><!-- <span>php echo  $_SESSION['usuario']['email']; ?></span> -->
+            </div>
 
-                <div class="grupoInformacoes">
-                    <label for="aniversario">Aniversário:</label>
-                    <span> 07/01/2003</span>
-                </div>
+            <div class="grupoInformacoes">
+                <label for="aniversario">Aniversário:</label>
+                <span> 07/01/2003</span>
+            </div>
 
-                <div class="grupoInformacoes">
-                    <label for="destinoFav">Destino Favorito:</label>
-                    <span> Roma, Florença3</span>
-                </div>
+            <div class="grupoInformacoes">
+                <label for="destinoFav">Destino Favorito:</label>
+                <span> Roma, Florença3</span>
+            </div>
 
-                <div class="grupoInformacoes">
-                    <label for="redesSociais">Redes Sociais: </label>
-                    <img src="../Public/Imagens/iconeInstagram.png" alt="Ícone do Instagram por Icons8">
-                </div>
+            <div class="grupoInformacoes">
+                <label for="redesSociais">Redes Sociais: </label>
+                <img src="../Public/Imagens/iconeInstagram.png" alt="Ícone do Instagram por Icons8">
+                <img src="../Public/Imagens/IconGmail.png" alt="Ícone do Instagram por Icons8">
+            </div>
 
-                <div class="grupoInformacoes">
-                    <button type="button" class="botaoEditarPerfil">Editar Perfil</button>
+            <div class="grupoInformacoes">
+                <button type="button" class="botaoEditarPerfil">Editar Perfil</button>
+            </div>
+        </div>
+        <div class="apresentacao-usuario">
+            <div class="titulo-apresentacao">
+                <h1>Anna Karolynna</h1><!-- <span>php echo  $_SESSION['usuario']['email']; ?></span> -->
+            </div>
+
+            <div class="botaoTipoAventureiro">
+                <button type="button" class="botaoTipoAventureiro"> Mochileira Novata </button>
+            </div>
+
+            <div class="apresentacao">
+                <p> Olá, exploradores do mundo! Eu sou a Anna, e minha paixão por desbravar o planeta vai além das fronteiras geográficas, permitindo-me mergulhar em cada cultura que encontro.
+                    Como uma viajante com espírito aventureiro, busco incessantemente novas experiências, que vão desde trilhas desafiadoras nas majestosas montanhas até momentos de pura tranquilidade em praias paradisíacas. A habilidade de me
+                    comunicar em diversos idiomas, como inglês, frances e espanhol, acrescenta uma dimensão extra ás minhas viagens, tornando cada jornada ainda mais emocionante.
+                </p>
+            </div>
+        </div>
+        <div class="cards">
+            <div class="menuHorizontal active">
+                <ul>
+                    <li><a href="#">Minhas Contribuições</a></li>
+                    <li><a href="#">Diários de Viagem</a></li>
+                    <li><a href="#">Meus Desafios</a></li>
+                    <li><a href="#">Meus Eventos</a></li>
+                    <li><a href="#">Salvos</a></li>
+                </ul>
+                <hr>
+            </div>
+            <div class="cartoes">
+                <div class="card1">
+                    <p>Diário de Viagem - Costa Rica 2024</p>
+                </div>
+                <div class="card2">
+                    <p>Diário de Viagem - Itália 2020</p>
+                </div>
+                <div class="card3">
+                    <p>Diário de Viagem - NovaYork 2020</p>
+                </div>
+                <div class="card4">
+                    <p>Diário de Viagem - Portugal 2021</p>
+                </div>
+                <div class="card5">
+                    <p>Diário de Viagem - Israel 2021</p>
+                </div>
+                <div class="card6">
+                    <p>Diário de Viagem - Canadá 2021</p>
+                </div>
+                <div class="card7">
+                    <p>Diário de Viagem - China 2022</p>
+                </div>
+                <div class="card8">
+                    <p>Diário de Viagem - Curitiba 2023</p>
+                </div>
+                <div class="card9">
+                    <p>Diário de Viagem - Alemanha 2023</p>
                 </div>
             </div>
-            <div class="descricao-membro"></div>
-            <div class="area-cards"></div>
+        </div>
     </article>
 
 </body>
