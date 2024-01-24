79a0e2ef112fd63971d783275e97c55cc00bd143
carol-sf <annacarolina.franca21@gmail.com>
2024-01-24 08:59:48 -0300

    Refactor: separando o css do menu

--- View/pagina-inicial.php
+++ View/pagina-inicial.php
@@ -11,6 +11,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href='../Public/CSS/Menu.css'>
     <link rel="stylesheet" href='../Public/CSS/PaginaInicial.css'>
     <link rel="stylesheet" href='../Public/CSS/Evento.css'>
 </head>
