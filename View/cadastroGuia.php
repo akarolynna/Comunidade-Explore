4318ceeaaae8fd87a0a21264206c94751e952709
carol-sf <annacarolina.franca21@gmail.com>
2024-01-24 10:58:56 -0300

    Feat, style: criando e estilizando a tela de cadastro de evento

--- View/cadastroGuia.php
+++ View/cadastroGuia.php
@@ -12,7 +12,7 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href='../Public/CSS/Menu.css'>
-    <link rel="stylesheet" href='../Public/CSS/CadastroGuia.css'>
+    <link rel="stylesheet" href='../Public/CSS/CadastroGeral.css'>
 </head>
 
 <body class="pagina">
