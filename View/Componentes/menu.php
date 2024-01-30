b43a33005481740881c708aa425792946dd4089e
Anna Karolynna <annakarolynnasouza8181@gmail.com>
2024-01-30 19:27:19 -0300

    corrigindo erros
    resolvendo erros

--- View/Componentes/menu.php
+++ View/Componentes/menu.php
@@ -1,4 +1,3 @@
-
 <nav class="menu">
     <a class="grupo" href="./pagina-inicial.php">
         <img src="../Public/Imagens/LogoExplore.png" alt="Logo de bussola" class="logo">
@@ -6,7 +5,7 @@
     </a>
     <div class="grupo">
         <button class="btn btn-success btn-sm botao" data-toggle="modal" data-target="#modalCriarPublicacao">Criar publicação</button>
-          
+
         <div class="modal" id="modalCriarPublicacao">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -15,32 +14,32 @@
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
                     <div class="buttonContainer">
-                        <a href="./CadastroDiarioViagem.php" class="button" id="">
+                        <a href="./CadastroDiarioViagem.php" class="button botaoDiario" id="btnOpcaoPublicacaoDiario">
                             <img src="./../Public/Imagens/pena-icone-verde.png" alt="opcao publicação" class="w-50">
                             <span>Diário</span>
                         </a>
-                        <a href="./cadastroEvento.php" class="button">
+                        <a href="./cadastroEvento.php" class="button botaoDiario ">
                             <img src="./../Public/Imagens/evento-icone-verde.png" alt="opcao publicação" class="w-50">
                             <span>Evento</span>
                         </a>
-                        <a href="./cadastroGuia.php" class="button">
+                        <a href="./cadastroGuia.php" class="button botaoDiario">
                             <img src="./../Public/Imagens/guia-icone-verde.png" alt="opcao publicação" class="w-50">
                             <span>Guia</span>
                         </a>
-                    </div> 
+                    </div>
                 </div>
             </div>
         </div>
-        
+
 
         <div class="dropdown-menu-left">
             <img src="<?php
-                if(false) {
-                echo $_SESSION['usuario']['foto'];
-            } else {
-                echo "../Public/Imagens/ImagemUsuario.png";
-            }  
-            ?>" alt="Foto do usuário" class="imagemUsuario dropdown-toggle" id="imagemUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        if (false) {
+                            echo $_SESSION['usuario']['foto'];
+                        } else {
+                            echo "../Public/Imagens/ImagemUsuario.png";
+                        }
+                        ?>" alt="Foto do usuário" class="imagemUsuario dropdown-toggle" id="imagemUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <div class="dropdown-menu" aria-labelledby="imagemUsuario">
                 <a href="./perfilUsuario.php" class="dropdown-item">Meu perfil</a>
                 <a href="./logout.php" class="dropdown-item">Sair</a>
@@ -48,8 +47,3 @@
         </div>
     </div>
 </nav>
-
-
-
-
-      
