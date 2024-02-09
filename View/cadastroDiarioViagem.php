0b7e77c0098eceed72ffb1e93567c045b15bb6e8
Anna Karolynna <annakarolynnasouza8181@gmail.com>
2024-02-09 02:42:36 -0300

    CSS E HTML CadGeral
    Ajustando css do cadastro geral e adicionando novo menu nocadastro de diario

--- View/cadastroDiarioViagem.php
+++ View/cadastroDiarioViagem.php
@@ -13,7 +13,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href='../Public/CSS/Menu.css'>
+    <link rel="stylesheet" type="text/css" href="../Public/CSS/menu2.css">
     <link rel="stylesheet" href='../Public/CSS/CadastroGeral.css'>
     <link rel="stylesheet" href='../Public/CSS/InputHora.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
@@ -23,9 +23,7 @@ session_start();
 <body>
 
     <body class="pagina">
-        <?php
-        require_once './Componentes/menu.php';
-        ?>
+        <?php require_once './Componentes/menu2.php'; ?>
 
         <div class="blocoPrincipal cadastroViagem">
             <h2 class="titulo">Crie um Diário de Viagem</h2>
@@ -51,5 +49,8 @@ session_start();
             </form>
     </body>
     <script src="../Public/JS/cadastroDiario.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </html>
--- View/cadastroEvento.php
+++ View/cadastroEvento.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    require_once '../Model/Categoria.enum.php';
+session_start();
+require_once '../Model/Categoria.enum.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href='../Public/CSS/Menu.css'>
+    <link rel="stylesheet" type="text/css" href="../Public/CSS/menu2.css">
     <link rel="stylesheet" href='../Public/CSS/CadastroGeral.css'>
     <link rel="stylesheet" href='../Public/CSS/InputHora.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
@@ -20,10 +20,9 @@
 </head>
 
 <body class="pagina">
-    <?php        
-        require_once './Componentes/menu.php';
-    ?>
- 
+<?php require_once './Componentes/menu2.php'; ?>
+
+
     <div class="blocoPrincipal">
         <h2 class="titulo">Crie um Evento</h2>
         <form id="formCadEvento">
@@ -40,7 +39,7 @@
                     <legend>Inicio</legend>
                     <div class="inputContainer">
                         <label class="label" for="inputDataInicio">Data</label>
-                        <input id="inputDataInicio" name="dataInicio" class="form-control" type="date" required/>
+                        <input id="inputDataInicio" name="dataInicio" class="form-control" type="date" required />
                     </div>
                     <div class="inputContainer">
                         <label class="label" for="inputHoraInicio">Hora</label>
@@ -58,7 +57,7 @@
                     <legend>Término</legend>
                     <div class="inputContainer">
                         <label class="label" for="inputDataTermino">Data</label>
-                        <input id="inputDataTermino" name="dataTermino" class="form-control" type="date" required/>
+                        <input id="inputDataTermino" name="dataTermino" class="form-control" type="date" required />
                     </div>
                     <div class="inputContainer">
                         <label class="label" for="inputHoraTermino">Hora</label>
@@ -82,7 +81,7 @@
                 <br>
                 <input type="file" class="form-control-file" id="inputFotoCapa" name="fotoCapa">
             </div>
-            
+
             <label class="label mt-3">Categoria</label>
             <select class="form-select mt-2" id="selectCategoria" name="categoria">
                 <option value="<?php echo Categoria::getValor(Categoria::PRAIA); ?>">
@@ -117,14 +116,14 @@
                 </option>
                 </option>
             </select>
-            
+
             <div class="form-check mt-4">
                 <input class="form-check-input" type="checkbox" id="checkMaxParticipantes">
                 <label class="checkLabel" for="checkMaxParticipantes">
                     Capacidade máxima de participantes
                 </label>
             </div>
-            <input type="number" min="0" class="form-control mb-3" id="inputMaxParticipantes" name="maxParticipantes" value="0"/>
+            <input type="number" min="0" class="form-control mb-3" id="inputMaxParticipantes" name="maxParticipantes" value="0" />
 
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="checkColaboradores">
@@ -134,7 +133,7 @@
             </div>
             <select multiple class="form-control" id="multiselectColaboradores"></select>
         </form>
-        
+
         <div class="d-flex justify-content-center mt-5 buttonContainer">
             <button class="btn btnSecundary mx-2" id="btnCancelar">Cancelar</button>
             <button class="btn btnPrimary" id="btnPublicar">Publicar</button>
