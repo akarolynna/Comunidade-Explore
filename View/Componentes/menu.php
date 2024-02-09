858172d8c6f3b416584fbbf07dec8d28baf7a12b
Anna Karolynna <annakarolynnasouza8181@gmail.com>
2024-02-08 23:54:15 -0300

    Corrigindo Foto Menu
    Ajustei o código da foto do usuário no menu, pois é necessário que o usuário tenha uma foto cadastrada antes de realizar qualquer outra ação. Portanto, o if anterior estava redundante, justificando assim a minha alteração.

--- View/Componentes/menu.php
+++ View/Componentes/menu.php
@@ -2,7 +2,7 @@
     <a class="grupo" href="./pagina-inicial.php">
         <img src="../Public/Imagens/LogoExplore.png" alt="Logo de bussola" class="logo">
         <h1 class="titulo">Comunidade Explore</h1>
-</a>
+    </a>
     <div class="grupo">
         <button class="btn btn-success btn-sm botao" data-toggle="modal" data-target="#modalCriarPublicacao">Criar publicação</button>
 
@@ -31,10 +31,17 @@
                 </div>
             </div>
         </div>
-
-
         <div class="dropdown-menu-left">
-            <img src="<?php
+            <img src="<?php echo $_SESSION['usuario']['foto'] ?>" alt=" Foto de usuário" class="imagemUsuario dropdown-toggle" id="imagemUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div class="dropdown-menu" aria-labelledby="imagemUsuario">
+                <a href="./perfilUsuario.php" class="dropdown-item">Meu perfil</a>
+                <a href="./logout.php" class="dropdown-item">Sair</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="dropdown-menu-left">
+            <img src="php
                         if (false) {
                             echo $_SESSION['usuario']['foto'];
                         } else {
@@ -46,5 +53,5 @@
                 <a href="./logout.php" class="dropdown-item">Sair</a>
             </div>
         </div>
-    </div>
+    </div> -->
 </nav>
